57539186fdc0d85642a6855989972ed7b9bdfb9b
Zhagrin <Z0rr083@mail.ru>
2024-05-13 13:25:50 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['яблоко', 'груша', 'виноград', 'дыня', 'киви']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'апельсин'
+print(my_list)
+my_dict = {'Dog':'Собака','Cat':'Кошка','Elefant':'Слон','Mouse':'Мышь'}
+print(my_dict)
+print(my_dict['Dog'])
+my_dict['Horse'] = 'Лошадь'
+print(my_dict)
